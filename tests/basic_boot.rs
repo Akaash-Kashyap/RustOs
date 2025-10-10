0b67faaf18b35a04d99f34d253a62251345097de
Akaash Kashyap <akaashkashyap@gmail.com>
2025-10-10 07:12:55 -0700

    Add interrupt handling, testing framework, and panic handling improvements

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,35 +4,25 @@
 #![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
+
 use os::println;
+use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    os::init();
-
-    x86_64::instructions::interrupts::int3();
-    
-    #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
-    
     loop {}
 }
 
 /// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
